1802dee05c33030306ac36523801faf7104439ee
Viet <viet@redone>
2015-02-27 19:40:30 -0800

    Hp is decrement now

--- entities.js
+++ entities.js
@@ -677,7 +677,7 @@ FireBall.prototype.update = function () {
     if (distance < 100) {
         this.explosion = true;
         if (this.add1) {
-          //  this.game.entities[3].hpBar += 0.4;
+          this.game.hp -= 10 //  this.game.entities[3].hpBar += 0.4;
             this.add1 = false;
         }
     }
