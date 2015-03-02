e2b8e7f1248ff271c758d68956cafbf0e66fa7b9
Red One <tcss491@gmail.com>
2015-03-02 03:21:24 -0800

    Update main.js

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ ASSET_MANAGER.downloadAll(function () {
     // enemy spaceship
     var enemy = new Enemy(gameEngine);
     var enemy2 = new Enemy2(gameEngine);
-    var enemy3 = new Enemy3(gameEngine);
+    var enemy3 = new SmallCraft(gameEngine, 3);
 
     // background
     // var bg = new ScrollBG1(gameEngine);
